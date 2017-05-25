7b0fabeca82347bdfa55b50f83c6ba5722b4003b
Metodi Obetsanov <htc.sens1@gmail.com>
2017-05-25 19:23:48 +0300

    Upload

--- Programming Fundamentals/04. CSharp Conditional Statements and Loops - Exercises/05. Word in Plural/Word in Plural.cs
+++ Programming Fundamentals/04. CSharp Conditional Statements and Loops - Exercises/05. Word in Plural/Word in Plural.cs
@@ -8,21 +8,13 @@ namespace _05.Word_in_Plural
         static void Main()
         {
             string word = Console.ReadLine();
-            if (word.EndsWith("y"))
-            {
-                
-                
-            }
-            else if (word.EndsWith("o") |ch","s","sh","x","z"))
-            {
-                
-            }
-            else
-            {
-                
-            }
 
+            if (word.EndsWith("y")) { word = word.Remove(word.Length - 1) + "ies"; }
+            else if (word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") || word.EndsWith("sh")
+                || word.EndsWith("x") || word.EndsWith("z")) { word += "es"; }
+            else { word += "s"; }
+            
             Console.WriteLine(word);
         }
     }
-}
+}S
