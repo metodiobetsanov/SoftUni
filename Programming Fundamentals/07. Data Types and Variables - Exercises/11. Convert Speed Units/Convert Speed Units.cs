12526cc6506e026271fb4e287515a0529f7f2f1b
Metodi Obetsanov <htc.sens1@gmail.com>
2017-05-30 20:30:00 +0300

    Update

---
+++ Programming Fundamentals/07. Data Types and Variables - Exercises/11. Convert Speed Units/Convert Speed Units.cs
@@ -0,0 +1,21 @@
+﻿namespace _11.Convert_Speed_Units
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            // to do 
+            var distance = double.Parse(Console.ReadLine());
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+            var seconds = int.Parse(Console.ReadLine());
+            double kmh = (distance / 1000) / (hours + (minutes / 60.0) + ((seconds / 60.0) / 60.0));
+            double ms = distance / (((hours * 60) * 60) + (minutes * 60) + seconds);
+            double mh = (distance / 1609) / (hours + (minutes / 60.0) + ((seconds / 60.0) / 60.0));
+
+            Console.WriteLine("{0}\n{1}\n{2}", Math.Round(ms,6), Math.Round(kmh, 6), Math.Round(mh, 6));
+        }
+    }
+}
