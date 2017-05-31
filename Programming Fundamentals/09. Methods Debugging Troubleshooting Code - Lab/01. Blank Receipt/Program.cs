0510d405a89f43d1a2bbe9f3bfa2f5fe198f192b
Metodi Obetsanov <htc.sens1@gmail.com>
2017-05-31 20:58:25 +0300

    Upload

---
+++ Programming Fundamentals/09. Methods Debugging Troubleshooting Code - Lab/01. Blank Receipt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Blank_Receipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
