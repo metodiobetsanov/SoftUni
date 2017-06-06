41f43bc7aeec8880a3e622da596cbd95de61fe34
Metodi Obetsanov <htc.sens1@gmail.com>
2017-06-06 18:02:22 +0300

    Update

---
+++ Programming Fundamentals/10. Methods Debugging Troubleshooting - Exercises/01. Hello, Name!/Hello, Name!.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.Hello__Name_
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            SayHello(name);
+        }
+
+        private static void SayHello(string name)
+        {
+            Console.WriteLine("Hello, {0}!", name);
+        }
+    }
+}
---
+++ Programming Fundamentals/10. Methods Debugging Troubleshooting - Exercises/02. Max Method/Max Method.cs
@@ -0,0 +1,28 @@
+﻿namespace _02.Max_Method
+{
+    using System;
+
+    public class Program
+    {
+        public static double GetMax(double numberOne, double numberTwo)
+        {
+            if (numberOne > numberTwo)
+            {
+                return numberOne;
+            }
+            else
+            {
+                return numberTwo;
+            }
+        }
+
+        public static void Main()
+        {
+            var numberOne = double.Parse(Console.ReadLine());
+            var numberTwo = double.Parse(Console.ReadLine());
+            var numberThree = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}", GetMax(GetMax(numberOne, numberTwo), numberThree));
+        }
+    }
+}
