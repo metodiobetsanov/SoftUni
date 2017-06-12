42cee03dcd11f45fdc3442b5e154576b10c0efa6
Metodi Obetsanov <htc.sens1@gmail.com>
2017-06-12 19:54:05 +0300

    update

---
+++ Programming Fundamentals/Lists - Lab/01. Remove Negatives and Reverse/Remove Negatives and Reverse.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace _01.Remove_Negatives_and_Reverse
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            List<int> myList = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (myList[i] < 0 )
+                {
+                    myList.Remove(myList[i]);
+                    i -= 1;
+                }
+            }
+
+            if (myList.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                myList.Reverse();
+                Console.WriteLine(string.Join(" ", myList));
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lists - Lab/02. Append Lists/Append Lists.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Append_Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Trim().Split('|').ToArray();
+            List<int> myList = new List<int>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                var temp = input[i].Trim().Split(' ').Select(int.Parse).ToArray();
+                {
+                    for (int j = 0; j < temp.Length; j++)
+                    {
+                        if (temp[j] != ' ')
+                        {
+                            myList.Add(temp[j]);
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", myList));
+        }
+    }
+}
