33cbdab39da115d1f58e9d8335d3e9235aa6cdf3
metodiobetsanov <htc.sens1@gmail.com>
2017-09-13 15:04:11 +0300

    Update

--- Python/01. First Steps in Coding/04. Triangle of 55 Stars.py
+++
@@ -1,3 +0,0 @@
-for x in range(0,10):
-    x += 1
-    print('*' * x)
--- Python/01. First Steps in Coding/06. Square of Stars.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-
-print('*' * n)
-for x in range(0, n-2):
-    x =+ 1
-    print('*' + (' ' * (n-2)) + '*')
-print('*' * n)
