0361eb3389a9c44f2e968f599dff99ef49360c75
Metodi Obetsanov <htc.sens1@gmail.com>
2018-01-04 16:40:16 +0200

    Upload DS

---
+++ Data Structures/Linear-Data-Structures/Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        ArrayList<int> list = new ArrayList<int>();
+        list.Add(10);
+
+        Console.WriteLine();
+    }
+}
