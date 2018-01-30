885c627ac99652ce4cebd55d0a92df5838075028
Metodi Obetsanov <htc.sens1@gmail.com>
2018-01-30 13:30:27 +0200

    Upload
    BashSoft

---
+++ C# Advanced/BashSoft/BashSoft/Launcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BashSoft
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
