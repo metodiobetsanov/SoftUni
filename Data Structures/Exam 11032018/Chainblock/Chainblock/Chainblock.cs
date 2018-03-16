c466dd8862e89dd15fbeca8e422028836c4a8bc0
Metodi Obetsanov <htc.sens1@gmail.com>
2018-03-16 17:52:28 +0200

    Upload
    DS Exams

---
+++ Data Structures/Exam 11032018/Chainblock/Chainblock/Chainblock.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Wintellect.PowerCollections;
+
+public class Chainblock : IChainblock
+{
+    private Dictionary<int, Transaction> byIds;
+
+    public Chainblock()
+    {
+        byIds = new Dictionary<int, Transaction>();
+    }
+
+    public int Count => byIds.Count;
+
+    public void Add(Transaction tx)
+    {
+        byIds.Add(tx.Id, tx);
+    }
+
+    public void ChangeTransactionStatus(int id, TransactionStatus newStatus)
+    {
+        if (!byIds.ContainsKey(id))
+        {
+            throw new ArgumentException();
+        }
+        byIds[id].Status = newStatus;
+    }
+
+    public bool Contains(Transaction tx)
+    {
+        if (byIds.ContainsKey(tx.Id))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool Contains(int id)
+    {
+        if (byIds.ContainsKey(id))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public IEnumerable<Transaction> GetAllInAmountRange(double lo, double hi)
+    {
+        var result = byIds.Where(x => x.Value.Amount >= lo && x.Value.Amount <= hi).Select(x => x.Value);
+
+        return result;
+    }
+
+    public IEnumerable<Transaction> GetAllOrderedByAmountDescendingThenById()
+    {
+        return byIds.Select(x => x.Value).OrderByDescending(x => x.Amount).ThenBy(x => x.Id);
+    }
+
+    public IEnumerable<string> GetAllReceiversWithTransactionStatus(TransactionStatus status)
+    {
+        var result = byIds.Values.Where(t => t.Status == status);
+
+        if (result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result.OrderByDescending(t => t.Amount).Select(t => t.To);
+    }
+
+    public IEnumerable<string> GetAllSendersWithTransactionStatus(TransactionStatus status)
+    {
+        var result = byIds.Values.Where(t => t.Status == status);
+
+        if (result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result.OrderByDescending(t => t.Amount).Select(t => t.From);
+    }
+
+    public Transaction GetById(int id)
+    {
+        if (!byIds.ContainsKey(id))
+        {
+            throw new InvalidOperationException();
+        }
+
+        return byIds[id];
+    }
+
+    public IEnumerable<Transaction> GetByReceiverAndAmountRange(string receiver, double lo, double hi)
+    {
+        if (!byIds.Values.Any(x => x.To == receiver))
+        {
+            throw new InvalidOperationException();
+        }
+
+        var result = byIds.Values.Where(x => x.To == receiver && x.Amount >= lo && x.Amount < hi);
+
+        return result.OrderByDescending(x => x.Amount).ThenBy(x => x.Id);
+    }
+
+    public IEnumerable<Transaction> GetByReceiverOrderedByAmountThenById(string receiver)
+    {
+        if (!byIds.Values.Any(x => x.To == receiver))
+        {
+            throw new InvalidOperationException();
+        }
+
+        var result = byIds.Values.Where(x => x.To == receiver).OrderByDescending(x => x.Amount).ThenBy(x => x.Id);
+
+        return result;
+    }
+
+    public IEnumerable<Transaction> GetBySenderAndMinimumAmountDescending(string sender, double amount)
+    {
+        var result = byIds.Values.Where(t => t.From == sender);
+        result =  result.Where(t => t.Amount > amount).OrderByDescending(x => x.Amount);
+        if (result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result;
+    }
+
+    public IEnumerable<Transaction> GetBySenderOrderedByAmountDescending(string sender)
+    {
+        if (!byIds.Values.Any(x => x.From == sender))
+        {
+            throw new InvalidOperationException();
+        }
+
+        var result = byIds.Values.Where(x => x.From == sender).OrderByDescending(x => x.Amount);
+        if (result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result;
+    }
+
+    public IEnumerable<Transaction> GetByTransactionStatus(TransactionStatus status)
+    {
+        if (!byIds.Values.Any(x => x.Status == status))
+        {
+            throw new InvalidOperationException();
+        }
+        var result = byIds.Values.Where(t => t.Status == status);
+        if (result.Count() == 0)
+        {
+            throw new InvalidOperationException();
+        }
+
+        return result.OrderByDescending(x => x.Amount);
+
+    }
+
+    public IEnumerable<Transaction> GetByTransactionStatusAndMaximumAmount(TransactionStatus status, double amount)
+    {
+        var result = new List<Transaction>();
+        if (!byIds.Values.Any(x => x.Status == status))
+        {
+            return result;
+        }
+
+        result = byIds.Values.Where(t => t.Status == status).Where(t => t.Amount <= amount).ToList();
+
+        return result.OrderByDescending(x => x.Amount);
+    }
+
+    public IEnumerator<Transaction> GetEnumerator()
+    {
+        foreach (var ts in byIds.Values)
+        {
+            yield return ts;
+        }
+    }
+
+    public void RemoveTransactionById(int id)
+    {
+        if (!byIds.ContainsKey(id))
+        {
+            throw new InvalidOperationException();
+        }
+        Transaction ts = byIds[id];
+        byIds.Remove(id);
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
---
+++ Data Structures/Exam 11032018/Instock/PeshoAndCo/Instock.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Wintellect.PowerCollections;
+using System.Linq;
+
+public class Instock : IProductStock
+{
+    private Dictionary<string, Product> byLabels;
+    private Dictionary<int, List<Product>> byqty;
+
+    private OrderedSet<Product> products;
+    private List<Product> byInsertaion;
+    public Instock()
+    {
+        byLabels = new Dictionary<string, Product>();
+        byqty = new Dictionary<int, List<Product>>();
+        byInsertaion = new List<Product>();
+        products = new OrderedSet<Product>();
+    }
+
+    public int Count => byInsertaion.Count;
+
+    public void Add(Product product)
+    {
+        byLabels.Add(product.Label, product);
+        byInsertaion.Add(product);
+        products.Add(product);
+        if (!byqty.ContainsKey(product.Quantity))
+        {
+            byqty.Add(product.Quantity, new List<Product>());
+        }
+
+        byqty[product.Quantity].Add(product);
+    }
+
+    public void ChangeQuantity(string product, int quantity)
+    {
+        if (!byLabels.ContainsKey(product))
+        {
+            throw new ArgumentException();
+        }
+
+        Product p = byLabels[product];
+        byqty[p.Quantity].Remove(p);
+        p.Quantity = quantity;
+        if (!byqty.ContainsKey(quantity))
+        {
+            byqty.Add(quantity, new List<Product>());
+        }
+        byqty[quantity].Add(p);
+
+        byLabels[product].Quantity = quantity;
+    }
+
+    public bool Contains(Product product)
+    {
+        if (byLabels.ContainsKey(product.Label))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Product Find(int index)
+    {
+        if (index < 0 || index >= Count)
+        {
+            throw new IndexOutOfRangeException();
+        }
+
+        return byInsertaion[index];
+    }
+
+    public IEnumerable<Product> FindAllByPrice(double price)
+    {
+        var result = byInsertaion.Where(p => p.Price == price);
+        return result;
+    }
+
+    public IEnumerable<Product> FindAllByQuantity(int quantity)
+    {
+        var result = new List<Product>();
+        if (!byqty.ContainsKey(quantity))
+        {
+            return result;
+        }
+
+        return byqty[quantity];
+
+    }
+
+    public IEnumerable<Product> FindAllInRange(double lo, double hi)
+    {
+        var result = byLabels.Where(p => p.Value.Price > lo && p.Value.Price <= hi).Select(p => p.Value).OrderByDescending(p => p.Price);
+        return result;
+    }
+
+    public Product FindByLabel(string label)
+    {
+        if (!byLabels.ContainsKey(label))
+        {
+            throw new ArgumentException();
+        }
+        return byLabels[label];
+    }
+
+    public IEnumerable<Product> FindFirstByAlphabeticalOrder(int count)
+    {
+        var result = products.Take(count);
+        if (products.Count < count)
+        {
+            throw new ArgumentException();
+        }
+        return result;
+    }
+
+    public IEnumerable<Product> FindFirstMostExpensiveProducts(int count)
+    {
+        var result = byInsertaion.OrderByDescending(x => x.Price).Take(count);
+        if (result.Count() < count)
+        {
+            throw new ArgumentException();
+        }
+
+        return result;
+    }
+
+    public IEnumerator<Product> GetEnumerator()
+    {
+        foreach (var item in byInsertaion)
+        {
+            yield return item;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
