c329d3488c3d77333ba465279935a55d343af06f
Metodi Obetsanov <htc.sens1@gmail.com>
2018-06-04 20:38:21 +0300

    upload

---
+++ C# Web/Web Basics/Workshop/WebServer/WebServer/Launcher.cs
@@ -0,0 +1,25 @@
+﻿using HTTPServer.Server;
+using HTTPServer.Server.Routing;
+
+namespace HTTPServer
+{
+    internal class Launcher
+    {
+        private static void Main(string[] args)
+        {
+            Run(args);
+        }
+
+        private static void Run(string[] args)
+        {
+            //TODO: Initialize application
+
+            var appRouteConfig = new AppRouteConfig();
+            //TODO: Configure App Route Configuration
+
+            var server = new WebServer(5252, appRouteConfig);
+
+            server.Run();
+        }
+    }
+}
