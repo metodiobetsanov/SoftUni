4a5f2650dbab399b24ee08877042e0022da2b10d
Metodi Obetsanov <htc.sens1@gmail.com>
2018-10-13 23:21:05 +0300

    Upload

---
+++ C# Web/Web Basics/SIS/SIS.HTTP/Contracts/IHttpCookieCollection.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace SIS.HTTP.Contracts
+{
+    using SIS.HTTP.Cookies;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    public interface IHttpCookieCollection
+    {
+        void Add(HttpCookie cookie);
+
+        bool ContainsCookies(string key);
+
+        bool HasCookies();
+
+        HttpCookie GetCookie(string key);
+    }
+}
---
+++ C# Web/Web Basics/SIS/SIS.HTTP/Cookies/HttpCookieCollection.cs
@@ -0,0 +1,56 @@
+﻿
+
+namespace SIS.HTTP.Cookies
+{
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Contracts;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    public class HttpCookieCollection : IHttpCookieCollection
+    {
+        private readonly IDictionary<string, HttpCookie> data;
+
+        public HttpCookieCollection()
+        {
+            this.data = new Dictionary<string, HttpCookie>();
+        }
+
+        public void Add(HttpCookie cookie)
+        {
+            CoreValidator.ThrowIfNull(cookie, nameof(cookie));
+
+            this.data[cookie.Key] = cookie;
+        }
+
+        public bool ContainsCookies(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            return this.data.ContainsKey(key);
+        }
+
+        public HttpCookie GetCookie(string key)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+
+            if (!this.ContainsCookies(key))
+            {
+                return null;
+            }
+
+            return this.data[key];
+        }
+
+        public bool HasCookies()
+        {
+            return this.data.Any();
+        }
+
+        public override string ToString()
+        {
+            return string.Join( ";", this.data.Values);
+        }
+    }
+}
